2311bcbe74533737b1a14e3e0ef1bcbf383c536d
lukasevicromuald <lukasevicromuald@gmail.com>
2022-11-10 23:54:09 +0200

    baig darbas

---
+++ P18_Baigiamasis_Darbas/script/_script.js
@@ -0,0 +1,4 @@
+
+document.getElementById("login").onclick = function(){window.location.href = "login.html"}
+document.getElementById("register").onclick = function(){window.location.href = "register.html"}
+
---
+++ P18_Baigiamasis_Darbas/script/register.js
@@ -0,0 +1,3 @@
+
+document.getElementById("back").innerHTML.onclick = function(){window.location.href = "mainpage.html"}
+
