e4a24342bc9d64fc12208440a7c322d599de3bcc
lukasevicromuald <lukasevicromuald@gmail.com>
2022-11-14 18:46:25 +0200

    Biagiamasis darbas 20221114 changes

--- P18_Baigiamasis_Darbas/script/register.js
+++ P18_Baigiamasis_Darbas/script/register.js
@@ -1,3 +1,21 @@
 
-document.getElementById("back").innerHTML.onclick = function(){window.location.href = "mainpage.html"}
+document.getElementById("back").onclick = function(){window.location.href = "mainpage.html"}
 
+
+var fname = document.getElementById("fname");
+var lname = document.getElementById("lname");
+var email = document.getElementById("email");
+var user2 = document.getElementById("user2");
+var pass2 = document.getElementById("pass2");
+var user = {
+fname:fname,
+lname:lname,
+email:email}
+
+
+function saveData() {
+    localStorage.setItem('fname', JSON.stringify(fname.value));
+    localStorage.setItem('lname', JSON.stringify(lname.value));
+    localStorage.setItem('email', JSON.stringify(email.value));
+    localStorage.setItem('user', JSON.stringify(user));   
+}
