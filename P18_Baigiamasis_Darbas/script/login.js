c8ad903b177b3ae4b1cfd2bd55e88a4f39cd9afe
lukasevicromuald <lukasevicromuald@gmail.com>
2022-11-14 18:45:57 +0200

    Baigiamasis darbas 20221114

--- P18_Baigiamasis_Darbas/script/login.js
+++ P18_Baigiamasis_Darbas/script/login.js
@@ -1,3 +1,15 @@
 
-document.getElementById("back").onclick = function(){location.href = "mainpage.html"}
+document.getElementById("back").onclick = function(){window.location.href = "mainpage.html"}
 
+
+
+
+function login() {
+    var checkuser = localStorage.getItem("username");
+    var checkpass = localStorage.getItem("password");
+    if (checkuser === user2.value && checkpass === pass2.value) {
+        document.getElementById("demo").innerHTML = "You are now logged in.";
+    } else {
+        document.getElementById("demo").innerHTML = "Incorrect username and password";
+    }
+}
